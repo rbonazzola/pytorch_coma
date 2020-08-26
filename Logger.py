521cdee8e869637a1b913710989aae6e2278a1f4
Rodrigo Bonazzola <rodbonazzola@gmail.com>
2020-08-26 18:36:35 +0100

    Changed location of logs

--- Logger.py
+++ Logger.py
@@ -7,7 +7,7 @@ from datetime import datetime
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 repo_root = check_output(shlex.split("git rev-parse --show-toplevel")).strip().decode('ascii')
-log_file = os.path.join(repo_root, 'output/log/%s.log' % timestamp)
+log_file = os.path.join(repo_root, 'output/%s/log' % timestamp)
 
 logging.basicConfig(
     level=logging.INFO,
