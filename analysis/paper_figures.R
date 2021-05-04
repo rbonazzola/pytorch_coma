7ea2ee6fe71ca863190e99747e14f949b08dde32
Rodrigo Bonazzola <rodbonazzola@gmail.com>
2021-05-04 08:08:51 +0100

    Provided a way to map original latent variable names to new ones, and change sign

--- analysis/paper_figures.R
+++ analysis/paper_figures.R
@@ -1,42 +1,50 @@
-source("relation_to_other_variables.R")
+source("analysis/relation_to_other_variables.R")
 
-run_ids <- c(
-  "experiment_1"="2020-09-11_02-13-41", 
-  "experiment_2"="2020-09-30_12-36-48"
-)
+run_params <- read_yaml("analysis/paper_z_mapping_parameters.yaml")
 
-generate_health_outcomes_fig <- function(run_id) {
-  df <- t_test_icd10(run_id, pval_as_log10=TRUE, logp_thres=8)
-  max_per_row <- apply(df, 1, FUN=max)
+generate_health_outcomes_fig <- function(run_id, mapping, sign, label) {
+  df <- t_test_icd10(run_id, pval_as_log10=TRUE, logp_thres=8, mapping, sign)
+  max_per_row <- apply(df, MARGIN = 1, FUN=max)
+  
   # print(min_per_row)
+  
   df <- df[max_per_row > 2,]
+  
   # print(df)
-  pp <- corrplot::corrplot(as.matrix(df), is.corr = FALSE, method = "number")
+  ttest_mat <- as.matrix(df)
+  colnames(ttest_mat) <- label # c(":z[1]^(s)", ":z[2]^(s)", ":z[3]^(s)", ":z[4]^(s)", ":z[5]^(s)", ":z[6]^(s)", ":z[7]^(s)", ":z[8]^(s)")
+  pp <- corrplot::corrplot(ttest_mat, is.corr = FALSE, method = "number", tl.srt = 90)
   pp
 }
 
 
-generate_figures <- function(run_id, alias=run_id) {
+generate_figures <- function(run_id, alias=run_id, mapping, sign, label) {
   root_dir <- system(command="git rev-parse --show-toplevel", intern=TRUE)
     
   png(file.path(root_dir, glue::glue("output/{run_id}/{alias}_vs_cardiac_indices.png")), res = 100)
-  pp <- corrplot::corrplot(corr_cardiac_indices(run_id))
+  corrmat <- corr_cardiac_indices(run_id, mapping, sign)
+  colnames(corrmat) <- label #(":z[1]", ":z[2]", ":z[3]", ":z[4]", ":z[5]", ":z[6]", ":z[7]", ":z[8]")
+  pp <- corrplot::corrplot(corrmat, tl.srt = 90)
   print(pp)
   dev.off()
 
   png(file.path(root_dir, glue::glue("output/{run_id}/{alias}_vs_demographic_data.png")), width=600, height=300)
-  pp <- corrplot::corrplot(corr_demographic_data(run_id))
-  print(pp)
+  corrmat <- corr_demographic_data(run_id, mapping, sign)
+  colnames(corrmat) <- label # c(":z[1]", ":z[2]", ":z[3]", ":z[4]", ":z[5]", ":z[6]", ":z[7]", ":z[8]")
+  pp <- corrplot::corrplot(corrmat, tl.srt = 90)
+  # print(pp)
   dev.off()
   
   png(file.path(root_dir, glue::glue("output/{run_id}/{alias}_health_outcome_t-test.png")), width=900, height=1200)
-  generate_health_outcomes_fig(run_id)
+  generate_health_outcomes_fig(run_id, mapping, sign, label)
   dev.off()
 }
 
 # CORRELATION WITH CARDIAC INDICES
-for (i in seq_along(run_ids)) {
-  run_id <- run_ids[i]
-  alias <- names(run_ids)[i]
-  generate_figures(run_id, alias)
+for (run_id in names(run_params)) {
+  alias <- run_params[[run_id]]$alias
+  mapping <- run_params[[run_id]]$mapping
+  sign <- run_params[[run_id]]$sign
+  label <- run_params[[run_id]]$label
+  generate_figures(run_id, alias, mapping, sign, label)
 }
